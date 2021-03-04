25cb25ccf0c18da96ae57b02fd722344959d56e3
Pawel Gertner <c0079988@newcastle.ac.uk>
2021-03-04 18:34:42 +0000

    Add InitialisingRooms, InitialisingStaff class

---
+++ src/main/Initialising_sample_data/InitialisingRooms.java
@@ -0,0 +1,36 @@
+import csc1035.project2.HibernateUtil;
+import csc1035.project2.Rooms;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import java.io.IOException;
+import java.util.List;
+
+public class InitialisingRooms {
+    public static void main(String args[]) throws IOException {
+        CsvIntoList l = new CsvIntoList();
+        List<List> data = l.readData("src/main/resources/rooms.csv");
+        for (int i = 1; i < data.size(); i++) {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            try {
+                session = HibernateUtil.getSessionFactory().openSession();
+                session.beginTransaction();
+                Rooms r = new Rooms(data.get(i).get(0).toString(),
+                        data.get(i).get(1).toString(),
+                        Integer.parseInt(data.get(i).get(2).toString()),
+                        Integer.parseInt(data.get(i).get(3).toString()));
+                session.saveOrUpdate(r);
+                session.getTransaction().commit();
+                }
+            catch (HibernateException e) {
+                if (session != null) session.getTransaction().rollback();
+                e.printStackTrace();
+                System.out.println("Rollback");
+            }
+            finally {
+                session.close();
+            }
+        }
+    }
+}
+
---
+++ src/main/Initialising_sample_data/InitialisingStaff.java
@@ -0,0 +1,35 @@
+import csc1035.project2.HibernateUtil;
+import csc1035.project2.Staff;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import java.io.IOException;
+import java.util.List;
+
+public class InitialisingStaff {
+    public static void main(String args[]) throws IOException {
+        CsvIntoList l = new CsvIntoList();
+        List<List> data = l.readData("src/main/resources/staff.csv");
+        for (int i = 1; i < data.size(); i++) {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            try {
+                session = HibernateUtil.getSessionFactory().openSession();
+                session.beginTransaction();
+                Staff s = new Staff(data.get(i).get(0).toString(),
+                        data.get(i).get(1).toString(),
+                        data.get(i).get(2).toString());
+                session.saveOrUpdate(s);
+                session.getTransaction().commit();
+                }
+            catch (HibernateException e) {
+                if (session != null) session.getTransaction().rollback();
+                e.printStackTrace();
+                System.out.println("Rollback");
+            }
+            finally {
+                session.close();
+            }
+        }
+    }
+}
+
